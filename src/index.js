1257b9e777f80b5c15841a28a946366a60c3af7e
draichi <lucasdraichi@gmail.com>
2021-06-13 03:19:30 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
-import "assets/demo/demo.css";
+// import "assets/demo/demo.css";
 
 import Index from "views/Index.js";
 import LandingPage from "views/examples/LandingPage.js";
