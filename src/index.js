19d770e84ea9c3f8996dad8db131d535f59ea3ba
draichi <lucasdraichi@gmail.com>
2021-07-03 19:54:47 -0300

    refactor: updates paths

--- src/index.js
+++ src/index.js
@@ -17,36 +17,16 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
-// import "assets/demo/demo.css";
-
-import Index from "views/Index.js";
-import LandingPage from "views/examples/LandingPage.js";
 import Gloxis from "views/examples/Gloxis.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/components" render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={(props) => <LandingPage {...props} />}
-      />
-      <Route path="/gloxis" render={(props) => <Gloxis {...props} />} />
-      <Route
-        path="/register-page"
-        render={(props) => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={(props) => <ProfilePage {...props} />}
-      />
-      <Redirect from="/" to="/components" />
+      <Route path="/" render={(props) => <Gloxis {...props} />} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
